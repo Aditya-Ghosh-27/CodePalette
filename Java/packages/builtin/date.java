1209be5d2a933af8f8d22c570214ee9b61f22fd9
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-06-06 00:13:29 +0530

    added built-in packages

---
+++ Java/packages/builtin/date.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class date{
+    public static void main(String[] args) {
+        Date ob = new Date();
+        System.out.println("Today's date is: " + ob);
+    }
+}
---
+++ Java/packages/builtin/lowertoUpper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class lowertoUpper {
+    public static void main(String[] args) {
+        String s1 = "Aditya";
+        String s2 = "ghosh";
+        String s3 = s1.toUpperCase();
+        String s4 = s2.toUpperCase();
+        System.out.println(s3 + " " + s4);
+    }
+}
