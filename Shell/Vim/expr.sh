01a9799e2b5d6f6eb0b7178e0e60e936edaedef3
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-06-06 20:59:09 +0530

    added basic shell programs

---
+++ Shell/Vim/expr.sh
@@ -0,0 +1,8 @@
+a=10
+b=20
+sum=$((a+b))
+echo "10 + 20: $sum"
+
+add=`expr $a + $b`
+echo 10 + 20 is $add
+
---
+++ Shell/Vim/tree.sh
@@ -0,0 +1,15 @@
+mkdir India
+cd India
+mkdir WB BH UP MP
+cd WB
+mkdir N24Pgns Birbhum Nodia
+cd N24Pgns
+mkdir Sodepur Agarpara Belghoria
+cd Agarpara
+mkdir Nit JISU
+cd .. 
+cd .. 
+cd ..
+cd ..
+tree India
+
